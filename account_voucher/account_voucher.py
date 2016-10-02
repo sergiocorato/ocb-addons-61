33476629ee57a5ba5ff6a2f1122ee6b60624b4be
sergio <sergiocorato@gmail.com>
2016-10-02 23:39:53 +0200

    [FIX] check account statement

--- account_voucher/account_voucher.py
+++ account_voucher/account_voucher.py
@@ -1458,17 +1458,17 @@ class account_bank_statement_line(osv.osv):
                 res[line.id] = 0.0
         return res
 
-    def _check_amount(self, cr, uid, ids, context=None):
-        for obj in self.browse(cr, uid, ids, context=context):
-            if obj.voucher_id:
-                diff = abs(obj.amount) - obj.voucher_id.amount
-                if not self.pool.get('res.currency').is_zero(cr, uid, obj.statement_id.currency, diff):
-                    return False
-        return True
-
-    _constraints = [
-        (_check_amount, 'The amount of the voucher must be the same amount as the one on the statement line', ['amount']),
-    ]
+    # def _check_amount(self, cr, uid, ids, context=None):
+    #     for obj in self.browse(cr, uid, ids, context=context):
+    #         if obj.voucher_id:
+    #             diff = abs(obj.amount) - obj.voucher_id.amount
+    #             if not self.pool.get('res.currency').is_zero(cr, uid, obj.statement_id.currency, diff):
+    #                 return False
+    #     return True
+    #
+    # _constraints = [
+    #     (_check_amount, 'The amount of the voucher must be the same amount as the one on the statement line', ['amount']),
+    # ]
 
     _columns = {
         'amount_reconciled': fields.function(_amount_reconciled,
